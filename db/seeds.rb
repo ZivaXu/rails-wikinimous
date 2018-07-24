e52da36f12e8574c5e6ce8f6f1675acac14397e7
Ziva Xu <yxxuziva@gmail.com>
2018-07-24 16:56:54 +0800

    In progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times {
+  Article.new(title: Faker::Simpsons.quote, content: Faker::BojackHorseman.tongue_twister).save
+}
